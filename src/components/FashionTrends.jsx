3c94b7e5f968eabbdb36c6f9671e9649be70199d
Mohammad Tahir <starghareeb@gmail.com>
2025-01-21 11:47:24 +0500

    latest trends image and description changed

--- src/components/FashionTrends.jsx
+++ src/components/FashionTrends.jsx
@@ -11,28 +11,16 @@ const FashionTrends = () => {
           <h2 className="title-font sm:text-4xl text-2xl mb-4 font-bold">Enjoy Our Latest Fashion Trends and Style
             {/* <br className="hidden lg:inline-block">readymade gluten */}
           </h2>
-          <p className="mb-3 leading-relaxed">Ship our curated collection of fashion items and stay ahead of the fashion game.
+          <p className="mb-3 leading-relaxed">High-quality office furniture enhances comfort and productivity, supporting long hours of work with ergonomic design. Durable and stylish, it also elevates the professional atmosphere of any workspace.
 
-            <div className="mt-8 flex flex-col md:flex-row justify-around items-center">
-              <div className="text-center md:text-left md:w-1/2 p-4">
-                <h3 className="text-xl font-semibold text-gray-800">Quality</h3>
-                <p className="text-gray-600">
-                  Our fashion items are crafted with the highest materials and attention to details.
-                </p>
-              </div>
-              <div className="text-center md:text-left md:w-1/2 p-4">
-                <h3 className="text-xl font-semibold text-gray-800">Style</h3>
-                <p className="text-gray-600">
-                  Express your unique style with our wide range of trendy items.
-                </p>
-              </div>
-            </div>
+         
           </p>
+          <p className="mb-3 leading-relaxed">Durable office furniture withstands daily use, maintaining its functionality and appearance over time. Investing in quality materials ensures long-term value and reduces the need for frequent replacements.    </p>
           <button className="px-4 py-3 bg-black text-white rounded-full "><Link to={"/products"} >Explore More &rarr;</Link> </button>
 
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img className="object-cover object-center rounded-lg lg:ml-24 lg:h-[25rem]" alt="hero" src="/fashion.png" />
+          <img className="object-cover object-center rounded-lg lg:ml-24 lg:h-[25rem]" alt="hero" src="/latest.jpg" />
         </div>
       </div>
     </section>
