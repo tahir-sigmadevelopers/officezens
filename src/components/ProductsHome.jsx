853e2424b517c4e57b230055872a2908882d71b2
Mohammad Tahir <starghareeb@gmail.com>
2025-02-03 17:40:34 +0500

    most of the done...

--- src/components/ProductsHome.jsx
+++ src/components/ProductsHome.jsx
@@ -38,12 +38,13 @@ const ProductsHome = () => {
             latestProducts && latestProducts?.map(product => (
               <div className="p-4 border rounded-lg shadow" key={product._id}>
                 <Link to={`/product/${product?._id}`} >
-                  <img src={product?.images &&  product?.images[0]?.url} alt={product?.name} className="w-full rounded-md" />
+                  <img src={product?.images && product?.images[0]?.url} alt={product?.name} className="w-full rounded-md" />
                 </Link>
                 <h3 className="mt-2 text-lg font-semibold mb-2 text-gray-800">{product?.name}</h3>
 
                 <h3 className="mt-2 text-lg font-semibold text-gray-800">${product?.price}</h3>
-                <p className="text-gray-600">{product?.description.slice(0,150)}...</p>
+                <p className="text-gray-600"> {product?.description?.slice(0, 100)}{product?.description?.length > 100 ? "..." : ""}
+                </p>
 
                 <button onClick={() => addToCartHandler(product._id, 1)} className="mt-2 px-4 py-2 bg-yellow-500 text-white rounded-full">Add to Cart</button>
               </div>
--- src/components/TrendsHome.jsx
+++ src/components/TrendsHome.jsx
@@ -32,29 +32,32 @@ const LatestTrends = () => {
       <p className="mt-2 text-gray-600 text-center">
         Stay updated with our information and engaging blog posts about modern furniture and fashion in the industry.
       </p>
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-6">
+      {latestLoading ? <Skeleton length={6} /> :
+        <>
+          <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-6">
 
 
-        {latestLoading ? <Skeleton /> : <>
-          {
-            oldItems && oldItems?.map(product => (
-              <div className="p-4 border rounded-lg shadow" key={product?._id}>
-                <Link to={`/product/${product?._id}`}>
-                  <img src={product?.images[0].url} alt="Trend" className="w-full rounded-md" />
-                </Link>
-                <h3 className="mt-2 text-lg font-semibold mb-2 text-gray-800">{product?.name}</h3>
-                <p className="text-gray-600">{product?.description.slice(0,150)}...</p>
-                <h3 className="mt-2 text-lg font-semibold text-gray-800">${product?.price}</h3>
+            {
+              oldItems && oldItems?.map(product => (
+                <div className="p-4 border rounded-lg shadow" key={product?._id}>
+                  <Link to={`/product/${product?._id}`}>
+                    <img src={product?.images[0].url} alt="Trend" className="w-full rounded-md" />
+                  </Link>
+                  <h3 className="mt-2 text-lg font-semibold mb-2 text-gray-800">{product?.name}</h3>
+                  <p className="text-gray-600">{product?.description?.slice(0, 100)}{product?.description?.length > 100 ? "..." : ""}
+                  </p>
+                  <h3 className="mt-2 text-lg font-semibold text-gray-800">${product?.price}</h3>
 
-                <button onClick={() => addToCartHandler(product._id, 1)} className="mt-2 px-4 py-2 bg-yellow-500 text-white rounded-full">Add to Cart</button>
-              </div>
-            ))}
+                  <button onClick={() => addToCartHandler(product._id, 1)} className="mt-2 px-4 py-2 bg-yellow-500 text-white rounded-full">Add to Cart</button>
+                </div>
+              ))
+            }
+  
+
+
+          </div>
         </>
-        }
-
-
-
-      </div>
+      }
     </section>
   );
 };
